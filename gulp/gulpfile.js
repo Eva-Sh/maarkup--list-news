fa1144ba024fbecbc3869e9ede087bf83a0c3348
macro cosm <macro cosm>
2017-11-02 17:15:15 +0300

    creat repo

---
+++ gulp/gulpfile.js
@@ -0,0 +1,105 @@
+var properties = {
+  port: 8080, // LiveReload server port
+  folders: {
+    build: '../public/theme', // Deploy folder
+    src: 'source', // Dev folder
+  }
+};
+
+var plugins = {
+  js: [
+    'bower_components/jquery/dist/jquery.min.js',
+  ],
+  css: [
+    'bower_components/normalize-scss/normalize.css',
+  ]
+};
+
+var
+  gulp = require('gulp'),
+  connect = require('gulp-connect'),
+  sourcemaps = require('gulp-sourcemaps'),
+  concat = require('gulp-concat'),
+  jade = require('gulp-jade'),
+  sass = require('gulp-sass'),
+  prefix = require('gulp-autoprefixer'),
+  babel = require('gulp-babel'),
+  minifyCSS = require('gulp-minify-css'),
+  watch = require('gulp-watch');
+
+function onError(err) {
+  console.log(err);
+  this.emit('end');
+}
+
+gulp.task('scripts', function() {
+  return gulp.src([
+      //properties.folders.src + '/scripts/funcName.js',
+      properties.folders.src + '/scripts/app.js',
+
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.js'))
+    .pipe(babel())
+    .on('error', onError)
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(properties.folders.build + '/scripts'))
+    .pipe(connect.reload());
+});
+
+gulp.task('vendor', function () {
+	gulp.src(plugins.css)
+	  .pipe(concat('vendor.css'))
+	  .pipe(gulp.dest(properties.folders.build + '/styles/'));
+	gulp.src(plugins.js)
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest(properties.folders.build + '/scripts/'));
+});
+
+gulp.task('jade', function() {
+	gulp.src(properties.folders.src + '/views/*.jade')
+		.pipe(jade({
+			pretty: true
+		}))
+    .on('error', onError)
+		.pipe(gulp.dest(properties.folders.build))
+    .on('end', function(){
+      gulp.src(properties.folders.build + '/**/*.html')
+        .pipe(connect.reload());
+    });
+});
+
+gulp.task('sass', function () {
+	gulp.src(properties.folders.src + '/styles/main.scss')
+    .pipe(sourcemaps.init())
+		.pipe(sass.sync().on('error', sass.logError))
+		.pipe(prefix("last 3 version", "> 1%", "ie 8", "ie 7"))
+    .pipe(sourcemaps.write('.'))
+    .pipe(minifyCSS({
+        keepBreaks: false // New rule will have break if 'true'
+      }))
+		.pipe(gulp.dest(properties.folders.build + '/styles'))
+		.pipe(connect.reload());
+});
+
+gulp.task('server', function() {
+  connect.server({
+    port: properties.port,
+		root: properties.folders.build,
+		livereload: true
+	});
+});
+
+gulp.task('watch', function() {
+	watch(properties.folders.src + '/views/**/*.jade', function() {
+    gulp.start('jade');
+	});
+	watch(properties.folders.src + '/styles/**/*.scss', function() {
+    gulp.start('sass');
+	});
+	watch(properties.folders.src + '/scripts/**/*.js', function() {
+    gulp.start('scripts');
+	});
+});
+
+gulp.task('default', ['server', 'jade', 'scripts', 'vendor', 'sass', 'watch']);
---
+++ public/theme/scripts/main.js
@@ -0,0 +1,49 @@
+"use strict";
+
+var testTradingView = {};
+
+testTradingView.showMore = function () {
+		$(".table-button").click(function () {
+				$('.table-line--hidden').show(250);
+		});
+};
+testTradingView.actionsFollowers = function () {
+		$(".top-line--icon").click(function () {
+				if ($(this).attr('data-status') === 'follow') {
+						$(this).attr('data-status', 'unfollow');
+						$(this).addClass('top-line--icon-add');
+						$(this).addClass('top-line--icon--unfollow');
+						$(this).removeClass('top-line--icon--follow');
+
+						var countFollowers = parseFloat($(this).parents('.table-line__item').parents('.table-master__line').find('.table-line__item--wrapper-row').find('.table-line__item--five').text());
+						var newQuantityFollowers = countFollowers + 1;
+						$(this).parents('.table-line__item').parents('.table-master__line').find('.table-line__item--wrapper-row').find('.table-line__item--five').text(newQuantityFollowers);
+				} else {
+						$(this).removeClass('top-line--icon-add');
+						$(this).attr('data-status', 'follow');
+						$(this).removeClass('top-line--icon--unfollow');
+						$(this).addClass('top-line--icon--follow');
+
+						var _countFollowers = parseFloat($(this).parents('.table-line__item').parents('.table-master__line').find('.table-line__item--wrapper-row').find('.table-line__item--five').text());
+						var _newQuantityFollowers = _countFollowers - 1;
+						$(this).parents('.table-line__item').parents('.table-master__line').find('.table-line__item--wrapper-row').find('.table-line__item--five').text(_newQuantityFollowers);
+				}
+		});
+
+		$('.top-line--icon').hover(function () {
+				if ($(this).attr('data-status') === 'follow') {
+						$(this).addClass('top-line--icon--follow');
+				} else {
+						$(this).addClass('top-line--icon--unfollow');
+				}
+		}, function () {
+				$(this).removeClass('top-line--icon--follow');
+				$(this).removeClass('top-line--icon--unfollow');
+		});
+};
+
+$(document).ready(function () {
+		testTradingView.showMore();
+		testTradingView.actionsFollowers();
+});
+//# sourceMappingURL=main.js.map
